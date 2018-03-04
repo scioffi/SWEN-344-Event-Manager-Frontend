71e4b6acd1e25727993426e6ae06b26209a253ff
Marianna Sternefeld <marianna@sternefeld.com>
2018-03-04 18:54:11 -0500

    Added some pages and such

--- src/components/Pages/Event.js
+++ src/components/Pages/Event.js
@@ -4,7 +4,22 @@ class Event extends React.Component {
     render() {
         return (
           <div>
-              Event
+              <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                    <h2>Event Name</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                    </div>
+                    <div class="col-md-8">
+                    <h2>Heading</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                    <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                                <a class="twitter-timeline"  href="https://twitter.com/hashtag/RIT" data-widget-id="968341462571274242">#RIT Tweets</a>
+                    </div>
+                </div>
+                </div>
           </div>
         );
     }
