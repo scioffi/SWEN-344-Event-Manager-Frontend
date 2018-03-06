a17db16cc3eb64b75ce148f105fe2dc9401dcffa
Marianna Sternefeld <marianna@sternefeld.com>
2018-03-05 20:41:54 -0500

    Per Stefano

--- src/components/Pages/Event.js
+++ src/components/Pages/Event.js
@@ -3,41 +3,69 @@ import React from "react";
 class Event extends React.Component {
 	constructor(props){
 		super(props);
-
-		window.alert(this.props.match.params.eventId);
+		this.state = {
+			fetching: true,
+			events: {}
+		}
+		//window.alert(this.props.match.params.eventId);
 	}
 	componentDidMount() {
+
+		const url = "http://localhost:8080/api/getEvent?eventId=" + this.props.match.params.eventId;
+		fetch(url, {
+			method: "get"
+		})
+		.then((res) => res.json())
+		.then((response) => {
+			console.log(response);
+			this.setState({
+				fetching: false,
+				events: response
+			});
+		})
+		.catch((error) => {
+			console.error(error);
+			// Should probably do some real error handling LOL
+		});
 		window.twttr.widgets.load();
 	}
-
 	render() {
-		return (
-			<div>
-				<div className="container">
-					<div className="row">
-						<div className="col-md-4">
-							<h2>Event Name</h2>
-							<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
-								commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam
-								porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-							<p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-						</div>
-						<div className="col-md-8">
-							<h2>Heading</h2>
-							<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
-								commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam
-								porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-							<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-								Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus
-								commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-							<p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-							<a className="twitter-timeline" href="https://twitter.com/hashtag/RIT" data-widget-id="968341462571274242">#RIT Tweets</a>
+		if (this.state.fetching === true){
+			return (
+				<div>
+					<h1>Loading Event...</h1>
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<div className="container">
+						<div className="row">
+							<div className="col-md-4">
+								<h2>{this.state.event.title}</h2>
+								<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
+									commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam
+									porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+								<p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+							</div>
+							<div className="col-md-8">
+								<h2>Heading</h2>
+								<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
+									commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam
+									porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+								<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+									Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus
+									commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+								<p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+								<a className="twitter-timeline" href="https://twitter.com/hashtag/RIT" data-widget-id="968341462571274242">#RIT Tweets</a>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		);
+			);
+		}
 	}
+	
 }
 
 export default Event;
