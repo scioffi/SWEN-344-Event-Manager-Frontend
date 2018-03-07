63f7a22ac4fae2b51a49fdfe2ea1bd8e206866ab
Stephen Cioffi <stephen@stephencioffi.com>
2018-03-07 14:42:13 -0500

    Spelt my name right

--- src/components/Pages/Messages.js
+++ src/components/Pages/Messages.js
@@ -8,8 +8,8 @@ class Messages extends React.Component {
         return(
             <div className="panel-group">
                 <div className="panel panel-default">
-                    <div className="panel-heading">Steven Cioffi Sent You an Event</div>
-                    <div className="panel-body">Steven Cioffi wants you to attend Spring Fest! </div>
+                    <div className="panel-heading">Stephen Cioffi Sent You an Event</div>
+                    <div className="panel-body">Stephen Cioffi wants you to attend Spring Fest! </div>
                     <div className="panel-footer"><a className="btn-sm btn-default" href="/Event/1" role="button">Go to Event &raquo;</a></div>
                 </div>
                 <div className="panel panel-default">
