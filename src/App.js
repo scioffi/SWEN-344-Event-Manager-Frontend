7611a3f92b69f737ad86131c32b45d6d01239e45
Marianna Sternefeld <marianna@sternefeld.com>
2018-03-08 19:45:08 -0500

    Added Edit Event

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
             <Route path="/Event/:eventId" component={Event} />
             <Route path="/EventList" component={ListOfEvents} />
             <Route path="/CreateEvent" component={CreateEvent} />
-            <Route path="/EditEvent" component={EditEvent} />
+            <Route path="/EditEvent/:eventId" component={EditEvent} />
             <Route  path="/Messages" component={Messages} />
           	<Route path="/Expired" component={Expired} />
             <Route path="/ListOfOrders" component={ListOfOrders} />
