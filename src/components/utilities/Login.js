406fc6abb2981ed0626dff977c887410f7876176
mesternefeld <marianna@sternefeld.com>
2018-04-23 19:15:55 -0400

    Made it a lot prettier

--- src/components/utilities/Login.js
+++ src/components/utilities/Login.js
@@ -41,13 +41,19 @@ export class Login extends React.Component{
 		if(!this.state.loggedIn) {
 			return (
 				<div>
-					<GoogleLogin
-						socialId="402862016858-cpmh4k9ajrf6le3v5h3726rs1sqllv97.apps.googleusercontent.com"
-						className="google-login"
-						fetchBasicProfile={true}
-						responseHandler={this.check}
-						buttonText="Login With Google"
-					/>
+					<div className="container">
+					<div class="center">
+						<h2>To sign into our app, please use Google.</h2>
+						
+						<GoogleLogin
+							socialId="402862016858-cpmh4k9ajrf6le3v5h3726rs1sqllv97.apps.googleusercontent.com"
+							className="google-login"
+							fetchBasicProfile={true}
+							responseHandler={this.check}
+							buttonText=""
+						/>
+						</div>
+					</div>
 				</div>
 			);
 		} else {
